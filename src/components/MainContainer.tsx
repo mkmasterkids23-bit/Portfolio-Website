551b547df76dcd97407b6884ab1d583294376d4a
mkmasterkids23-bit <mkmasterkids23@example.com>
2026-04-29 10:17:24 +0400

    Restored FooterFinale and improved scroll sequence

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -8,6 +8,7 @@ import Navbar from "./Navbar";
 import SocialIcons from "./SocialIcons";
 import WhatIDo from "./WhatIDo";
 import Work from "./Work";
+import FooterFinale from "./FooterFinale";
 import setSplitText from "./utils/splitText";
 
 const TechStack = lazy(() => import("./TechStack"));
@@ -42,7 +43,7 @@ const MainContainer = ({ children }: PropsWithChildren) => {
           <WhatIDo />
           <Career />
           <Work />
-          <div style={{ height: "300vh" }}></div>
+          <FooterFinale />
         </div>
       </div>
     </div>
